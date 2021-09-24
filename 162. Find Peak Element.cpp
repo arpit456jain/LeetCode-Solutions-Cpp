68a621f1762a1c8835f3de81b0d6182121560c76
Arpit <111arpit1@gmail.com>
2021-09-24 23:44:39 +0530

    update

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+    int optimizeApproch(vector<int>& arr,int low,int high, int n)
+	{
+		int mid = low + (high - low)/2 ;
+	    if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == n-1 || arr[mid+1] <= arr[mid]))
+        return mid;
+  
+
+        else if (mid > 0 && arr[mid-1] > arr[mid])
+            return optimizeApproch(arr, low, (mid -1), n);
+
+        else 
+            return optimizeApproch(arr, (mid + 1), high, n);
+
+	}
     int findPeakElement(vector<int>& arr) {
-        int pos=0;
-        int n=arr.size();
-      int maxele = arr[0];
-      for(int i=1;i<n;i++)
-      {
-          if(arr[i]>maxele)
-          {
-              maxele = arr[i];
-              pos = i;
-          }
-      }
-      return pos;
+        int ans;
+        int n = arr.size();
+        if(n<=1)
+            return 0;
+	  ans = optimizeApproch(arr,0,n-1,n);
+        cout<<ans;
+      return ans;
     }
 };
